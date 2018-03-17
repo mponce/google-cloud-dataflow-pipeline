93d87bc4ca5f70f53e36dbee89f3180b709256b6
Mario Ponce <mponce.cs@gmail.com>
2018-03-16 23:08:05 -0700

    starter pipeline

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# google-cloud-dataflow-demo
-Google Cloud DataFlow - Load CSV Files to BigQuery Tables
+# Introduction
+
+This is a demo on how to load data into Google BigQuery using Google Cloud DataFlow pipelines.
+
+Google Cloud DataFlow is an Apache Beam Pipeline runner. A Pipeline is a sequence of data transformations applied to PCollections of Elements of different ElementTypes.
+
+# Local Setup
+
+### Download and Install GCP SDK
+
+Download and install the Google Cloud SDK to your computer by running:
+
+```bash
+sudo curl https://sdk.cloud.google.com | bash
+```
+After installing the SDK you will have ```gcloud and gsutil``` commands to interact with GCP services.
+
+Make sure you login before compiling the project:
+```bash
+gcloud auth application-default login
+```
+This will open the browser and redirect you the GCP services to authenticate your computer.
+
+Optionally set your default project:
+
+```bash
+gcloud config set project <YOUR CLOUD PLATFORM PROJECT ID>
+```
+
+# Test Data
+
+To run this demo you need to have your GCP account setup. This code can run from the developer machine on the GCP or from local system with the GCP SDK installed. 
+
+You will also need to have some sample data in your Storage in CSV format to load to BigQuery. There is some sample data in the data folder of this project for reference.  
+
+
+# Compile and Run
+
+```bash
+mvn compile exec:java -Dexec.mainClass=com.example.DemoPipeline -Dexec.args="--project=<YOUR CLOUD PLATFORM PROJECT ID> \
+    --stagingLocation=<YOUR CLOUD STORAGE LOCATION> \
+    --output=<YOUR CLOUD STORAGE LOCATION> \
+    --jobName=<ANY JOB NAME> \
+    --templateLocation=<OPTIONALLY SAVE THIS JOB AS TEMPLATE AT STORAGE LOCATION>
+    --runner=DataflowRunner"  
+```
---
+++ src/main/java/com/example/DemoPipeline.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.Validation;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SimpleFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DemoPipeline {
+    private static final Logger LOG = LoggerFactory.getLogger(DemoPipeline.class);
+
+    public interface MyOptions extends PipelineOptions {
+        /**
+         * Set this required option to specify where to write the output.
+         */
+        @Description("Path of the file to write to")
+        @Validation.Required
+        String getOutput();
+        void setOutput(String value);
+    }
+
+    public static void main(String[] args) {
+        MyOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(MyOptions.class);
+        Pipeline p = Pipeline.create(options);
+
+        p.apply(Create.of("Hello", "World"))
+                .apply(MapElements.via(new SimpleFunction<String, String>() {
+                    @Override
+                    public String apply(String input) {
+                        return input.toUpperCase();
+                    }
+                }))
+                .apply(ParDo.of(new DoFn<String, Void>() {
+                    @ProcessElement
+                    public void processElement(ProcessContext c)  {
+                        LOG.info(c.element());
+                    }
+                }));
+
+        p.run();
+    }
+}
